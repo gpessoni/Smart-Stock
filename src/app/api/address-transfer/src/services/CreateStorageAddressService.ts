fe8e57e2d0a3b0cc27cfae7d7ceb7ca25aef21cc
gpessoni <pessonigs@gmail.com>
2024-09-07 22:01:51 -0300

    🗃️ raw: Update some products routes

--- src/app/api/address-transfer/src/services/CreateStorageAddressService.ts
+++ src/app/api/address-transfer/src/services/CreateStorageAddressService.ts
@@ -1,78 +1,86 @@
-import { prisma } from "@/app/api/config/prisma";
-import { NextResponse } from "next/server";
-import { HttpStatus } from "@/app/api/config/http/httpUtils";
-import { createTransferValidation } from "../validation";
+import { prisma } from "@/app/api/config/prisma"
+import { NextResponse } from "next/server"
+import { HttpStatus } from "@/app/api/config/http/httpUtils"
+import { createTransferValidation } from "../validation"
+import { ValidationError } from "joi"
 
 export async function createTransferService(req: Request) {
-  try {
-    const body = await req.json();
-    const { productId, fromAddressId, toAddressId, quantity } = body;
+    try {
+        const body = await req.json()
+        const { productId, fromAddressId, toAddressId, quantity } = body
 
-    const fromBalance = await prisma.productStorageBalance.findFirst({
-      where: {
-        productId,
-        storageAddressId: fromAddressId,
-      },
-    });
+        const { error } = createTransferValidation.validate(body, {
+            abortEarly: false,
+        })
 
-    if (!fromBalance || fromBalance.balance < quantity) {
-      return NextResponse.json(
-        {
-          error: `Saldo insuficiente para o produto no endereço de origem ${fromAddressId}`,
-        },
-        { status: HttpStatus.BAD_REQUEST }
-      );
+        if (error) {
+            const validationError = error as ValidationError
+            const errorMessage = validationError.details.map((detail) => detail.message).join(", ")
+            return NextResponse.json({ error: errorMessage }, { status: HttpStatus.BAD_REQUEST })
+        }
+
+        const fromBalance = await prisma.productStorageBalance.findFirst({
+            where: {
+                productId,
+                storageAddressId: fromAddressId,
+            },
+        })
+
+        if (!fromBalance || fromBalance.balance < quantity) {
+            return NextResponse.json(
+                {
+                    error: `Saldo insuficiente para o produto no endereço de origem ${fromAddressId}`,
+                },
+                { status: HttpStatus.BAD_REQUEST }
+            )
+        }
+
+        const toBalance = await prisma.productStorageBalance.findFirst({
+            where: {
+                productId,
+                storageAddressId: toAddressId,
+            },
+        })
+
+        if (!toBalance) {
+            await prisma.productStorageBalance.create({
+                data: {
+                    productId,
+                    storageAddressId: toAddressId,
+                    balance: quantity,
+                },
+            })
+        } else {
+            await prisma.productStorageBalance.update({
+                where: {
+                    id: toBalance.id,
+                },
+                data: {
+                    balance: toBalance.balance + quantity,
+                },
+            })
+        }
+
+        await prisma.productStorageBalance.update({
+            where: {
+                id: fromBalance.id,
+            },
+            data: {
+                balance: fromBalance.balance - quantity,
+            },
+        })
+
+        const transfer = await prisma.addressTransfer.create({
+            data: {
+                productId,
+                fromAddressId,
+                toAddressId,
+                quantity,
+            },
+        })
+
+        return NextResponse.json(transfer, { status: HttpStatus.CREATED })
+    } catch (error) {
+        return NextResponse.json({ message: "Erro no servidor", error: (error as Error).message }, { status: HttpStatus.INTERNAL_SERVER_ERROR })
     }
-
-    const toBalance = await prisma.productStorageBalance.findFirst({
-      where: {
-        productId,
-        storageAddressId: toAddressId,
-      },
-    });
-
-    if (!toBalance) {
-      await prisma.productStorageBalance.create({
-        data: {
-          productId,
-          storageAddressId: toAddressId,
-          balance: quantity,
-        },
-      });
-    } else {
-      await prisma.productStorageBalance.update({
-        where: {
-          id: toBalance.id,
-        },
-        data: {
-          balance: toBalance.balance + quantity,
-        },
-      });
-    }
-
-    await prisma.productStorageBalance.update({
-      where: {
-        id: fromBalance.id,
-      },
-      data: {
-        balance: fromBalance.balance - quantity,
-      },
-    });
-
-    const transfer = await prisma.addressTransfer.create({
-      data: {
-        productId,
-        fromAddressId,
-        toAddressId,
-        quantity,
-      },
-    });
-
-    return NextResponse.json(transfer, { status: HttpStatus.CREATED });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "Erro no servidor", error: (error as Error).message },
-      { status: HttpStatus.INTERNAL_SERVER_ERROR }
-    );
-  }
 }
--- src/app/api/products/src/services/ListProductsService.ts
+++ src/app/api/products/src/services/ListProductsService.ts
@@ -13,6 +13,11 @@ export async function listProductsService() {
                     include: {
                         storageAddress: true,
                     },
+                    where: {
+                        balance: {
+                            gt: 0,
+                        },
+                    },
                 },
             },
         })
